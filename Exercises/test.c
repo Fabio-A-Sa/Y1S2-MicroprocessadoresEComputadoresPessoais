119dfcd7362105c306d3212e8d6813dad73a50b5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-25 17:10:28 +0100

    test

--- Exercises/test.c
+++ Exercises/test.c
@@ -1,35 +1,15 @@
-// Created on February, 2021
-// @author: Fábio Araújo de Sá
+#include <stdio.h>
+#include <stdlib.h>
 
-include \masm32\include\masm32rt.inc	; use the Masm32 library
+extern int ContPar(long *array, long size);
 
-.code
-demomain:
-  REPEAT 20
-	switch rv(nrandom, 9)	; generate a number between 0 and 8
-	mov ecx, 7
-	case 0
-		print "case 0"
-	case ecx				; in contrast to most other programming languages,
-		print "case 7"		; the Masm32 switch allows "variable cases"
-	case 1 .. 3
-		.if eax==1
-			print "case 1"
-		.elseif eax==2
-			print "case 2"
-		.else
-			print "cases 1 to 3: other"
-		.endif
-	case 4, 6, 8
-		print "cases 4, 6 or 8"
-	default
-		mov ebx, 19		     ; print 20 stars
-		.Repeat
-			print "*"
-			dec ebx
-		.Until Sign?		 ; loop until the sign flag is set
-	endsw
-	print chr$(13, 10)
-  ENDM
-  exit
-end demomain
+int main(void) {
+    long size = 11;
+    long array[] = {0, 2, 3, 5, 2, 6, 1, 7, 8, 2, 10};
+
+    long evens = ContPar(array, size);
+
+    printf("Resultado: %ld", evens);
+
+    return 0;
+}
--- Exercises/test.s
+++ Exercises/test.s
@@ -1,35 +1,18 @@
-// Created on February, 2021
-// @author: Fábio Araújo de Sá
+.text
+.global ContPar
+.type ContPar, "function"
 
-include \masm32\include\masm32rt.inc	; use the Masm32 library
+ContPar:	mov X3, 0 // Counter
+           	mov X4, 0 // Offset
+           	mov x6, x1
 
-.code
-demomain:
-  REPEAT 20
-	switch rv(nrandom, 9)	; generate a number between 0 and 8
-	mov ecx, 7
-	case 0
-		print "case 0"
-	case ecx				; in contrast to most other programming languages,
-		print "case 7"		; the Masm32 switch allows "variable cases"
-	case 1 .. 3
-		.if eax==1
-			print "case 1"
-		.elseif eax==2
-			print "case 2"
-		.else
-			print "cases 1 to 3: other"
-		.endif
-	case 4, 6, 8
-		print "cases 4, 6 or 8"
-	default
-		mov ebx, 19		     ; print 20 stars
-		.Repeat
-			print "*"
-			dec ebx
-		.Until Sign?		 ; loop until the sign flag is set
-	endsw
-	print chr$(13, 10)
-  ENDM
-  exit
-end demomain
+loop:       cbz X1, final
+            ldr X2, [X0, X4]
+            and X5, X2, 1
+            add X3, X3, X5
+            add X4, X4, 8
+            sub X1, X1, 1
+            b loop
+
+final: 		sub x0, x6, x3
+            ret
