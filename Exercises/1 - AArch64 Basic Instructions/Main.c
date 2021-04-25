e23e4376c2fbaf7377c69ede5e724006c9dcab9c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-25 18:29:17 +0100

    Added assembly subroutine

--- Exercises/1 - AArch64 Basic Instructions/Main.c
+++ Exercises/1 - AArch64 Basic Instructions/Main.c
@@ -8,7 +8,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+extern int decrement(int number);
+
+void decrement_a_unit()
 {
-    return EXIT_SUCCESS;
+	printf("Exercise 1A, decrement a unit: \n");
+	int numbers[] = {-10, 5, 7, 2, 9, 2, 16, 5, 6, 8};
+	for (int i = 0 ; i < 10 ; i++) {
+		printf("Result of number %d is %d\n", numbers[i], decrement(numbers[i]));
+	}
+	printf("\n");
+}
+
+
+int main()
+{
+	decrement_a_unit();
+	return EXIT_SUCCESS;
 }
