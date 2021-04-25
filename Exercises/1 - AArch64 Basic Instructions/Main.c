1302c5d66db2ad697bb4b964d7f3ca43e8f82a83
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-25 18:29:26 +0100

    Added c project

--- Exercises/1 - AArch64 Basic Instructions/Main.c
+++ Exercises/1 - AArch64 Basic Instructions/Main.c
@@ -1,10 +1,6 @@
 // Created on February 21, 2021
 // @author: Fábio Araújo de Sá
 
-/*
-    This code was created to test many subroutines in Assembly language using Eclipse Framework.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
