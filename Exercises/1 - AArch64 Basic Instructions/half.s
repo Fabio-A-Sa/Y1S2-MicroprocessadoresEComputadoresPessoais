6c80781bc33854e81a8c8935740c85185b8d878e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-25 20:35:45 +0100

    Modify file

--- Exercises/1 - AArch64 Basic Instructions/half.s
+++ Exercises/1 - AArch64 Basic Instructions/half.s
@@ -2,12 +2,14 @@
 .global half
 .type half, "function"
 
-half: 	mov X7, 1
-		mov X2, 32
-		mov W1, W0
-		lsr X0, X0, X2
+// Verificar se os 32 bits mais significativos de X0 são iguais aos seus 32 bits menos significativos.
+
+half: 	MOV X7, #1
+		MOV X2, #32
+		MOV W1, W0
+		LSR X0, X0, X2
 		CMP W1, W0
 		B.EQ finish
-		mov x7, 0
-finish: mov x0, x7
-		ret
+		MOV X7, #0
+finish: MOV X0, X7
+		RET
