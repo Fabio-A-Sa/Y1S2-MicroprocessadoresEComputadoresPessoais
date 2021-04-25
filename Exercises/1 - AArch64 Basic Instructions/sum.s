9e492cd0563e47868e3bcd681ef23763f80ba8c3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-25 21:04:42 +0100

    sum

---
+++ Exercises/1 - AArch64 Basic Instructions/sum.s
@@ -0,0 +1,14 @@
+.text
+.global sum
+.type sum, "function"
+
+// Somar os valores 11, 21, 31,  ... 101 usando um ciclo
+
+sum: MOV X3, #0
+loop: CMP X0, X1
+	B.GT finish
+	ADD X3, X3, X0
+	ADD X0, X0, X2
+	B loop
+finish: MOV X0, X3
+	RET
