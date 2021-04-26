88d2d895b6e5094b72cf10f5493a91292f42996a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-26 22:50:29 +0100

    Add d

---
+++ Pratical Work/1 - Substitution by Zero/Tester.c
@@ -0,0 +1,10 @@
+/*
+Exercício de programação 1
+Escrever uma sub-rotina que substitui por 0 (zero) os elementos de uma sequência cujo valor absoluto é maior que o valor positivo X, devolvendo o número de elementos modificados. Os elementos da sequência são do tipo signed word.
+A sub-rotina deve ter o nome CheckABS e aceitar os seguintes argumentos pela ordem indicada:
+
+valor X (do tipo unsigned word);
+número de elementos da sequência (do tipo unsigned word);
+endereço-base da sequência.
+
+*/
