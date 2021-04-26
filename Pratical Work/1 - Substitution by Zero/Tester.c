99bc343a60d86b27adc495485643bdfb6e75ba4a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-26 22:53:27 +0100

    Add d

--- Pratical Work/1 - Substitution by Zero/Tester.c
+++ Pratical Work/1 - Substitution by Zero/Tester.c
@@ -2,11 +2,30 @@
 Exercício de programação 1
 Escrever uma sub-rotina que substitui por 0 (zero) os elementos de uma sequência cujo valor absoluto é maior que o valor positivo X, devolvendo o número de elementos modificados. 
 Os elementos da sequência são do tipo signed word. A sub-rotina deve ter o nome CheckABS (checkabs.s) e aceitar os seguintes argumentos pela ordem indicada:
-
 X0: valor X (do tipo unsigned word);
 X1: número de elementos da sequência (do tipo unsigned word);
 X2: endereço-base da sequência.
-
 */
 
-.
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int CheckABS( int numx, int tam, int *seq);
+
+int main(void) {
+    int x = 20; 
+    int tamanho = 8;
+    int ve[] = {7, -8, -23, 56, 20, -10, 0, 40};
+    int res;         
+
+    res = CheckABS(x, tamanho, ve);
+    printf("Foram modificados %d elementos da sequencia ve[]", res);
+
+    // para o exemplo fornecido o output deve indicar que foram alterados 3 elementos
+
+    // no final da execução a sequência ve[] = {7, -8, 0, 0, 20, -10, 0, 0} (podem verificar
+
+    //o seu conteúdo durante a execução em modo debug)
+
+    return EXIT_SUCCESS;
+}
---
+++ Pratical Work/1 - Substitution by Zero/checkabs.s
@@ -0,0 +1,5 @@
+.text
+.global CheckABS
+.type CheckABS, "function"
+
+CheckABS
