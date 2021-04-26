0eb4bccad0b0cb73a8e26beb5c857a8bfe12885f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-26 23:32:57 +0100

    Add tra

--- Pratical Work/1 - Substitution by Zero/checkabs.s
+++ Pratical Work/1 - Substitution by Zero/checkabs.s
@@ -2,4 +2,21 @@
 .global CheckABS
 .type CheckABS, "function"
 
-CheckABS
+CheckABS: MOV X3, #0
+	MOV X5, #0
+	MOV X6, #0
+loop: CBZ X1, finish
+	LDR X4, [X2, X5]
+	CMP X4, #0
+	B.GE jump1
+	NEG X4, X4
+jump1: CMP X4, X0
+	B.HI jump2
+	ADD X3, X3, #1
+	STR X6, [X2, X5]
+jump2: SUB X1, X1, #1
+	ADD X5, X5, #8
+	B loop
+
+finish: MOV X0, X3
+	RET
