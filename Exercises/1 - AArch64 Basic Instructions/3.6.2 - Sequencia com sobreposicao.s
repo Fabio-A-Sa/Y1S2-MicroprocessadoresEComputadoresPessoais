2c36da859fca2d5d144d4e4f6ab8fce2c899621e
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 13:23:15 +0100

    File

--- Exercises/1 - AArch64 Basic Instructions/3.6.2 - Sequencia com sobreposicao.s
+++ Exercises/1 - AArch64 Basic Instructions/3.6.2 - Sequencia com sobreposicao.s
@@ -6,15 +6,15 @@ sequence2:  MOV X2, 0
 	    MOV X3, 64
 	    MOV X7, #1
 
-loop:   CBZ X3, finish
-        SUB X3, X3, 1
-        MOV W4, 0
-        UBFX W4, W0, #0, #5
-        LSR X0, X0, X7
-        CMP W4, W1
-        B.NE loop
-        ADD X2, X2, #1
-        B loop
+loop:       CBZ X3, finish
+            SUB X3, X3, 1
+            MOV W4, 0
+            UBFX W4, W0, #0, #5
+            LSR X0, X0, X7
+            CMP W4, W1
+            B.NE loop
+            ADD X2, X2, #1
+            B loop
 
 finish:     MOV X0, X2
 	    RET
