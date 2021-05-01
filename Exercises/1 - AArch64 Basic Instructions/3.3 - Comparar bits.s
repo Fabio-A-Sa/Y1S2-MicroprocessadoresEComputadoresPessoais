b1047d4d03e7f6081a7b6e9a6020749818b63928
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 11:28:57 +0100

    File

---
+++ Exercises/1 - AArch64 Basic Instructions/3.3 - Comparar bits.s
@@ -0,0 +1,20 @@
+.text
+.global compair
+.type compair, "function"
+
+compair: MOV X2, #0
+		MOV X3, #1
+loop: AND X4, X0, X3
+		AND X5, X1, X3
+		EOR X6, X5, X4
+		CMP X6, 0
+		B.NE jump
+		ADD	X2, X2, X3
+jump: LSR X0, X0, X3
+		LSR X1, X1, X3
+		CMP X0, #0
+		B.NE loop
+		CMP X1, #0
+		B.NE loop
+		MOV X0, X2
+		RET
