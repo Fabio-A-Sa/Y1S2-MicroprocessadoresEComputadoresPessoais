77c87ad44c7f3935b74bd9d2f76d46bb217328c0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 11:30:22 +0100

    File

--- Exercises/1 - AArch64 Basic Instructions/3.4 - Pares num vetor.s
+++ Exercises/1 - AArch64 Basic Instructions/3.4 - Pares num vetor.s
@@ -5,14 +5,14 @@
 // Contar quantos números pares existem numa sequência de inteiros
 
 pairs: MOV X3, #0
-		MOV X4, #0
-   		MOV X6, x1
+	MOV X4, #0
+   	MOV X6, x1
 loop: CBZ X1, finish
-		LDR X2, [X0, X4]
-		AND X5, X2, #1
-		ADD X3, X3, X5
-		ADD X4, X4, #8
-		SUB X1, X1, #1
-		B loop
+	LDR X2, [X0, X4]
+	AND X5, X2, #1
+	ADD X3, X3, X5
+	ADD X4, X4, #8
+	SUB X1, X1, #1
+	B loop
 finish:	SUB X0, X6, X3
-		RET
+	RET
