484cef9bb3485eb89e97919a8fd7b81566cea5b2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 13:15:31 +0100

    File

---
+++ Exercises/1 - AArch64 Basic Instructions/3.6.1 - Sequencia sem sobreposicao.s
@@ -0,0 +1,20 @@
+.text
+.global sequence1
+.type sequence1, "function"
+
+sequence1: MOV X2, 0
+	MOV X3, 12 // math.floor(64/5)
+	MOV X7, #5
+
+loop: CBZ X3, finish
+	SUB X3, X3, 1
+	MOV W4, 0
+	UBFX W4, W0, #0, #5
+	LSR X0, X0, X7
+	CMP W4, W1
+	B.NE loop
+	ADD X2, X2, #1
+	B loop
+
+finish: MOV X0, X2
+	RET
