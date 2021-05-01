d8c03ec165080fccf25b96909a7450c7135fcef3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 11:23:25 +0100

    File

--- Exercises/1 - AArch64 Basic Instructions/1.2 - Maior valor.s
+++ Exercises/1 - AArch64 Basic Instructions/1.2 - Maior valor.s
@@ -2,7 +2,7 @@
 .global higher
 .type higher, "function"
 
-// Colocar no registo X0 o maior dos valores dos registos X0 e X11
+// Colocar no registo X0 o maior dos valores dos registos X0 e X1
 
 higher: 	CMP X0, X1;
 		B.GE finish
