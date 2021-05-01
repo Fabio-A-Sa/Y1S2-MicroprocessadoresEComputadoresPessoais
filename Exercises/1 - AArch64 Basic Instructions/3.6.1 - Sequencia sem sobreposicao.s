5f6c0474be7f844f7bef85507d364e7ae4600062
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 13:27:58 +0100

    File

--- Exercises/1 - AArch64 Basic Instructions/3.6.1 - Sequencia sem sobreposicao.s
+++ Exercises/1 - AArch64 Basic Instructions/3.6.1 - Sequencia sem sobreposicao.s
@@ -2,6 +2,8 @@
 .global sequence1
 .type sequence1, "function"
 
+Determinar quantas vezes a sequência ’10110’ aparece na representação binária do valor de X0 sem sobreposição
+
 sequence1:  MOV X2, 0
 	    MOV X3, 12 // math.floor(64/5)
 	    MOV X7, #5
