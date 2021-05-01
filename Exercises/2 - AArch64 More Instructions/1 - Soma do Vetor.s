b51e8b03eb38b8a4fa911551411f089f5d8a84dc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-01 15:01:01 +0100

    1

---
+++ Exercises/2 - AArch64 More Instructions/1 - Soma do Vetor.s
@@ -0,0 +1,13 @@
+.text
+.global SOMA_V
+.type SOMA_V, "function"
+
+SOMA_V:	MOV W5, 0
+loop: 	CBZ W1, finish
+	LDR X3, [X0]
+	ADD X5, X5, X3
+	ADD X0, X0, 4
+	SUB W1, W1, 1
+	B loop
+finish: MOV X0, X5
+	RET
