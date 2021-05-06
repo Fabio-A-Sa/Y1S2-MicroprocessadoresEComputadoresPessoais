34b04fcdd8d83e26948d2ccce2140bbe83472ee7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-06 15:41:10 +0100

    C code

--- Pratical Work/2 - String Uppercase/Tester.c
+++ Pratical Work/2 - String Uppercase/Tester.c
@@ -1,10 +1,12 @@
 /*
- * Escreva uma sub-rotina que aceita duas sequências: seq_A e seq_B. Os elementos de seq_B são todos distintos e apenas podem ser letras minúsculas, sem acentos ou outras alterações. A sub-rotina altera para a sua versão maiúscula, todos os carateres de seq_A que surgem na seq_B.
+ * Escreva uma sub-rotina que aceita duas sequências: seq_A e seq_B. Os elementos de seq_B são todos distintos e apenas podem ser letras minúsculas, sem acentos ou outras alterações. 
+ * A sub-rotina altera para a sua versão maiúscula, todos os carateres de seq_A que surgem na seq_B.
  * Apenas carateres que sejam letras minúsculas, sem acentos ou outras alterações, deverão ser alterados.
  * Para além de alterar a seq_A, a sub-rotina deve retornar o número de carateres de seq_A que são alterados (este valor é um inteiro sem sinal).
- * Sequências de carateres são terminadas pelo carater nulo, o qual é codificado com o número 0; Consulte uma tabela ASCII (ex: http://www.asciitable.com/). A sub-rotina deve ter o nome SelectedUpperCase e aceitar os seguintes argumentos pela ordem indicada:
-	endereço-base de seq_B;
-	endereço-base de seq_A.
+ * Sequências de carateres são terminadas pelo carater nulo, o qual é codificado com o número 0; Consulte uma tabela ASCII (ex: http://www.asciitable.com/). 
+ * A sub-rotina deve ter o nome SelectedUpperCase e aceitar os seguintes argumentos pela ordem indicada:
+	- endereço-base de seq_B;
+	- endereço-base de seq_A.
 */
 
 #include <stdio.h>
@@ -12,39 +14,42 @@
 
 extern unsigned int SelectedUpperCase(char *seq_B, char *seq_A);
 
-int solution (char seq_B[], char seq_A[]) {
+void test (char seq_A[], char seq_B[]) {
 
-	int result = 0;
-	for (int i = 0 ; i < sizeof(seq_B) ; i++ ) {
-		for (int j = 0 ; j < sizeof(seq_A) ; j++ ) {
-			if (seq_A[j] == seq_B[i]) {
-				result ++ ;
-			}
-		}
-	}
-
-	return result;
-}
-
-void test (char seq_B[], char seq_A[]) {
-
-	int sol = solution(seq_B, seq_A);
-	printf("\nInput: %s\n", seq_A);
-	unsigned int attemp = SelectedUpperCase(seq_B, seq_A);
-	printf("Output: %s\nKey: %s\nModifications: %d %d", seq_A, seq_B, attemp, sol);
-	if (sol == attemp) {
-		printf("PASSED\n");
-	}
-	else {
-		printf("NOT PASSED\n");
-	}
+	printf("\nTEST: \nInput: %s\n", seq_A);
+	unsigned int solution = SelectedUpperCase(seq_B, seq_A);
+	printf("Output: %s\nKey: %s\nResult: %d\n", seq_A, seq_B, solution);
 }
 
 int main()
 {
     char seq_A[] = "Esta frase deverá ter alguns dos seus carateres alterados para maiúscula!";
     char seq_B[] = "afuily";
+    test (seq_A, seq_B);
 
-    test(seq_B, seq_A);
+    char seq_C[] = "Qualquer CENA só para ver se funcioNA";
+    char seq_D[] = "simplekey";
+    test (seq_C, seq_D);
+
+    char seq_E[] = "             ";
+    char seq_F[] = "something";
+    test (seq_E, seq_F);
+
+    char seq_G[] = "Esta frase não deverá ter alguns dos seus carateres alterados para maiúscula!";
+    char seq_H[] = "";
+    test (seq_G, seq_H);
+
+    char seq_I[] = "ESTÁ TUDO EM MAÚSCULA, NÃO ALTERAR NADA!";
+    char seq_J[] = "abcdefghijklmnopqrstuvwxyz";
+    test (seq_I, seq_J);
+
+    char seq_K[] = "the quick brown fox jumps over the lazy dog";
+    char seq_L[] = "abcdefghijklmnopqrstuvwxyz";
+    test (seq_K, seq_L);
+
+    char seq_M[] = "Esta frase deverá ter alguns dos seus carateres alterados para maiúscula!";
+    char seq_N[] = "xwz";
+    test (seq_M, seq_N);
+
     return EXIT_SUCCESS;
 }
