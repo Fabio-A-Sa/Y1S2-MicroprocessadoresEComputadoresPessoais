f95501b77b4302c2002203a78726312f0d3616ba
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-08 19:02:52 +0100

    File

---
+++ Exercises/2 - AArch64 More Instructions/11.1 - Soma de vectors sem overflow.s
@@ -0,0 +1,17 @@
+.text
+.global VECTOR_SUM
+.type VECTOR_SUM, "function"
+
+// Adição de vetores – assumir que a soma é representável com o mesmo número de bits dos operandos, ou seja, assumir que não ocorre overow
+
+VECTOR_SUM: CBZ W2, FINISH
+	LDR W4, [X0]
+	LDR W5, [X1]
+	ADD W4, W4, W5
+	STR W4, [X0]
+	ADD X0, X0, 4
+	ADD X1, X1, 4
+	SUB W2, W2, 1
+	B VECTOR_SUM
+
+FINISH: RET
