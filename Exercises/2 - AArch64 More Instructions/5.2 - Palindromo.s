340069e20178d9b2eedfcd49ff80bd37021c0c68
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-08 18:21:08 +0100

    File

--- Exercises/2 - AArch64 More Instructions/5.2 - Palindromo.s
+++ Exercises/2 - AArch64 More Instructions/5.2 - Palindromo.s
@@ -2,7 +2,7 @@
 .global PAL1
 .type PAL1, "function"
 
-// Verificar se a representação binária de um número é capicua (número palíndromo). A resposta será 1 em caso armativo e 0 no caso contrário
+// Vericar se uma cadeia de 8 carateres (tamanho de uma doubleword) é palíndromo. A resposta será 1 em caso armativo e 0 no caso contrário
 
 PAL1: RBIT W1, W0
 	CMP W1, W0
