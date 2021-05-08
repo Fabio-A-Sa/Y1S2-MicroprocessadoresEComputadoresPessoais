b5c5d85f16c0883b1c8f2c8086423a06bbc4bf9d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-08 17:51:07 +0100

    File

--- Exercises/2 - AArch64 More Instructions/3.5 - Palindromo.s
+++ Exercises/2 - AArch64 More Instructions/3.5 - Palindromo.s
@@ -4,5 +4,27 @@
 
 // Implementar um programa que determine se uma cadeia de carateres é palíndromo
 
-PALIND: MOV X0, 0
+PALIND: MOV W6, 0
+	MOV X2, 0
+	MOV X7, 0
+
+CONT: LDRB W6, [X0]
+	CBZ W6, COPY
+	ADD W6, W6, 1
+	B CONT
+
+COPY: CBZ W6, COMPAIR
+	LDRB W3, [X0]
+	SUB X0, X0, 1
+	SUB W6, W6, 1
+	STRB w3, [X2]
+	ADD X2, X2, 1
+
+COMPAIR: LDRB W4, [X0]
+	LDRB W5, [X1]
+	CMP W4, W5
+	B.NE FINISH
+	ADD X7, X7, 1
+
+FINISH: MOV X0, X7
 	RET
