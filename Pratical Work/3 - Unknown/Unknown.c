4842cfd07b9d5c94eb4f8b58a519a1a88b1671c4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-22 12:23:27 +0100

    C

--- Pratical Work/3 - Unknown/Unknown.c
+++ Pratical Work/3 - Unknown/Unknown.c
@@ -5,47 +5,22 @@ extern int OpMat(char nc, char nl, unsigned char *o, unsigned char *v);
 
 int main(void)
 {
-	int  res;
-	char i, j;
-  unsigned char ncol = 3;  // Nº colunas da matriz
-  unsigned char nlin = 4;  // Nº linhas da matriz
-  unsigned char mat1[] = { 29, 127,  11, 255,
-                         100,   5, 200,   0,
-                           1,  29,   0, 128 };
+	  int  res;
+	  char i, j;
+	  unsigned char ncol = 4;  // Nº colunas da matriz
+	  unsigned char nlin = 3;  // Nº linhas da matriz
+	  unsigned char mat1[] = { 29, 127,  11, 255,
+							 100,   5, 200,   0,
+							   1,  29,   0, 128 };
 
-  unsigned char ops[] = {'C', 0, 33, 'X'};
-  res = OpMat(ncol, nlin, ops, mat1);
-  for (i=0; i<nlin; i++)
-  {
-      for (j=0; j<ncol; j++)
-          printf("%*d", 4, mat1[i*ncol+j]);
-      printf("\n");
-  }
+	  unsigned char ops[] = {'C', 1, 33, 'L', 2, 123, 'P', 0, 2, 99, 'X'};
+	  res = OpMat(ncol, nlin, ops, mat1);
+	  for (i=0; i<nlin; i++)
+	  {
+		  for (j=0; j<ncol; j++)
+			  printf("%*d", 4, mat1[i*ncol+j]);
+		  printf("\n");
+	  }
 
-  unsigned char ops2[] = {'C', 1, 0, 'X'};   // Exemplo a)
-  unsigned char mat2[] = { 29, 127,  11, 255,
-                         100,   5, 200,   0,
-                           1,  29,   0, 128 };
-
-  res = OpMat(ncol, nlin, ops2, mat2);
-  for (i=0; i<nlin; i++)
-  {
-      for (j=0; j<ncol; j++)
-          printf("%*d", 4, mat2[i*ncol+j]);
-      printf("\n");
-  }
-
-  unsigned char ops3[] = {'C', 2, 50, 'X'};   // Exemplo a)
-  unsigned char mat3[] = { 29, 127,  11, 255,
-                         100,   5, 200,   0,
-                           1,  29,   0, 128 };
-
-  res = OpMat(ncol, nlin, ops3, mat3);
-  for (i=0; i<nlin; i++)
-  {
-      for (j=0; j<ncol; j++)
-          printf("%*d", 4, mat3[i*ncol+j]);
-      printf("\n");
-  }
-  return EXIT_SUCCESS;
+	  return EXIT_SUCCESS;
 }
