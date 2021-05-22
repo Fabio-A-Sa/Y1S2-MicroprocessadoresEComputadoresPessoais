62a4983059518d0aad83a4d04260da399f0293ab
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-22 12:09:05 +0100

    C

--- Pratical Work/3 - Unknown/Unknown.s
+++ Pratical Work/3 - Unknown/Unknown.s
@@ -10,6 +10,8 @@ Loop: 		LDRB W4, [X2]
 			B.EQ Finish
 			CMP W4, 76
 			B.EQ option_L
+			CMP W4, 67
+			B.EQ option_C
 
 option_L:	ADD X2, X2, 1
 			LDRB W4, [X2]
@@ -23,6 +25,22 @@ Loop_L:		CMP W6, W7
 			ADD X6, X6, 1
 			B Loop_L
 Line_comp: 	ADD X2, X2, 1
+			B Loop
+
+
+
+option_C:	ADD X2, X2, 1
+			LDRB W4, [X2]
+			ADD X2, X2, 1
+			LDRB W5, [X2]
+			MOV W6, W4
+			MUL W7, W0, W1
+Loop_C:		CMP W6, W7
+			B.HI Col_comp
+			STRB W5, [X3, X6]
+			ADD X6, X6, X0
+			B Loop_C
+Col_comp: 	ADD X2, X2, 1
 			B Loop
 
 
