4668aaf77a2062a5227c7fd9dfc4437dff5e84b3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-22 22:40:02 +0100

    Main

--- Exercises/3 - VF instructions/Main.c
+++ Exercises/3 - VF instructions/Main.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 
 extern double op1(double B, double M, double N);
+extern double op2(double W, double X);
 
-void doub()
+void doub1()
 {
 	double B = 7.8, M = 3.6, N = 7.1;
 	double P = op1(B, M, N);
 	printf("\nAnswer is %f\n", P);
 }
 
+void doub2()
+{
+	double W = 7, X = 7.1;
+	double Y = op2(W, X);
+	printf("\nAnswer is %f\n", Y);
+}
+
 int main ()
 {
-	doub();
+	doub1();
+	doub2();
 	return 0;
 }
 
