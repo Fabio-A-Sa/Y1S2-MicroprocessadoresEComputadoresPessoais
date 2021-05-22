686c423e23b2e26bbecc7c50a7d32c2ffe4c8b16
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-22 12:09:24 +0100

    C

--- Pratical Work/3 - Unknown/Unknown.c
+++ Pratical Work/3 - Unknown/Unknown.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int  res;
 	char i, j;
-  unsigned char ncol = 4;  // Nº colunas da matriz
-  unsigned char nlin = 3;  // Nº linhas da matriz
+  unsigned char ncol = 3;  // Nº colunas da matriz
+  unsigned char nlin = 4;  // Nº linhas da matriz
   unsigned char mat1[] = { 29, 127,  11, 255,
                          100,   5, 200,   0,
                            1,  29,   0, 128 };
 
-  unsigned char ops[] = {'L', 0, 33, 'X'};
+  unsigned char ops[] = {'C', 0, 33, 'X'};
   res = OpMat(ncol, nlin, ops, mat1);
   for (i=0; i<nlin; i++)
   {
@@ -21,9 +21,8 @@ int main(void)
           printf("%*d", 4, mat1[i*ncol+j]);
       printf("\n");
   }
-  printf("\nE isto");
 
-  unsigned char ops2[] = {'L', 1, 0, 'X'};   // Exemplo a)
+  unsigned char ops2[] = {'C', 1, 0, 'X'};   // Exemplo a)
   unsigned char mat2[] = { 29, 127,  11, 255,
                          100,   5, 200,   0,
                            1,  29,   0, 128 };
@@ -35,9 +34,8 @@ int main(void)
           printf("%*d", 4, mat2[i*ncol+j]);
       printf("\n");
   }
-  printf("\nE isto");
 
-  unsigned char ops3[] = {'L', 3, 50, 'X'};   // Exemplo a)
+  unsigned char ops3[] = {'C', 2, 50, 'X'};   // Exemplo a)
   unsigned char mat3[] = { 29, 127,  11, 255,
                          100,   5, 200,   0,
                            1,  29,   0, 128 };
@@ -49,6 +47,5 @@ int main(void)
           printf("%*d", 4, mat3[i*ncol+j]);
       printf("\n");
   }
-  printf("\nE isto");
   return EXIT_SUCCESS;
 }
