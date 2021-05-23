19baa95d72566e7454f656cec1b1fa23ed6183b1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 10:39:40 +0100

    Tester

--- Exercises/3 - VF instructions/Main.c
+++ Exercises/3 - VF instructions/Main.c
@@ -3,6 +3,7 @@
 
 extern double op1(double B, double M, double N);
 extern double op2(double W, double X);
+extern double exp(int A, int B, int C, int D);
 
 void doub1()
 {
@@ -18,10 +19,18 @@ void doub2()
 	printf("\nAnswer is %f\n", Y);
 }
 
+void expression()
+{
+	int A = 1, B = 2, C = 3, D = 4;
+	double E = exp(A, B, C, D);
+	printf("\nAnswer is %f\n", E);
+}
+
 int main ()
 {
 	doub1();
 	doub2();
+	expression();
 	return 0;
 }
 
