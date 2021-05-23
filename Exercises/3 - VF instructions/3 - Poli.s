a05204590906dbf6eba304ca3ee3501356e2a816
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 13:08:42 +0100

    3

--- Exercises/3 - VF instructions/3 - Poli.s
+++ Exercises/3 - VF instructions/3 - Poli.s
@@ -1,7 +1,9 @@
 .data
 	a1: .float 1.5
-	a2: .float -12.5
+	a2: .float 12.5
 	a3: .float 7
+	increment: .float 0.1
+	initial: .float 0
 .text
 .global pol
 .type pol, "function"
@@ -9,19 +11,25 @@
 pol: LDR S0, a1
 	LDR S1, a2
 	LDR S2, a3
-	MOV X1, 10
+	LDR S3, increment
+	LDR S4, initial
+	MOV X1, 101
+
 loop: CBZ X1, Finish
-	LDR S5, [X0]
-	FMOV S7, S5
+
 	FMUL S6, S7, S7
-	FMUL S7, S7, S6
+	FMUL S7, S6, S7
 	FMUL S7, S7, S0
-	FMUL S5, S5, S1
-	FADD S4, S5, S2
-	FADD S4, S5, S7
-	STR S4, [X0]
-	ADD X0, X0, 1
+
+	FMUL S5, S4, S1
+	FSUB S7, S7, S5
+	FADD S7, S7, S2
+
+	STR S7, [X0]
+	ADD X0, X0, 8
+	FADD S4, S4, S3
 	SUB X1, X1, 1
 	B loop
 
 Finish: RET
+
