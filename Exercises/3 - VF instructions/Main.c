eddf2b3bca594088937f3be3020d226215a5641a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-23 10:47:27 +0100

    Main

--- Exercises/3 - VF instructions/Main.c
+++ Exercises/3 - VF instructions/Main.c
@@ -3,7 +3,8 @@
 
 extern double op1(double B, double M, double N);
 extern double op2(double W, double X);
-extern double exp(int A, int B, int C, int D);
+extern double expr(int A, int B, int C, int D);
+extern double area (int r);
 
 void doub1()
 {
@@ -22,8 +23,16 @@ void doub2()
 void expression()
 {
 	int A = 1, B = 2, C = 3, D = 4;
-	double E = exp(A, B, C, D);
+	double E = expr(A, B, C, D);
 	printf("\nAnswer is %f\n", E);
+}
+
+void area_circle()
+{
+	int r1 = 4;
+	int r2 = 7;
+	printf("Area of %d is %f\n", r1, area(r1));
+	printf("Area of %d is %f\n", r2, area(r2));
 }
 
 int main ()
@@ -31,6 +40,7 @@ int main ()
 	doub1();
 	doub2();
 	expression();
+	area_circle();
 	return 0;
 }
 
