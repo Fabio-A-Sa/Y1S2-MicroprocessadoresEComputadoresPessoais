86d3ed04b69f53fb32abe41c31f7317d578f0c3f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-27 15:06:51 +0100

    Closest circle

--- Pratical Work/4 - Circles/Tester.c
+++ Pratical Work/4 - Circles/Tester.c
@@ -3,28 +3,37 @@
 
 extern void closestCircle(unsigned int np, float *vp, unsigned int nc, double *vc, int *id);
 
-int main(void)
+void cout(unsigned int np, int ID[])
 {
-  unsigned int NP = 2;  // Nº pontos
-  unsigned int NC = 4;  // Nº circunferências
-  int ID[] = {-1, -1};
-  //Exemplo 1: VP1 e VC1
-  float VP1[] = {-1.0, 0.0, 2.7, 1.15};
-  double VC1[]= {-4.0, -1.0, 1.0, -3.89, 5.4, 0.51, 2.0, 1.0, 1.0, 3.0, 1.21, 0.9};
-  //Exemplo 2: VP2 e VC2
-  float VP2[] = {-3.0, 1.0, -3.0, -1.5};
-  double VC2[]= {0.0, -6.0, 6.0, -10.0, 0.0, 10.75, 0.0, 0.0, 5.0};
-  int i;
+	for (int i = 0 ; i < np ; i++ ) {
+		printf("%d ", ID[i]);
+	}
+	printf("\n");
+}
 
-  closestCircle(NP, VP1, NC, VC1, ID);
-  printf("Exemplo 1: ID = ");
-  for (i=0; i<NP; i++)
-     {printf("%d ", ID[i]); ID[i]=-1;}
-  printf("\n");
-  closestCircle(NP, VP2, 3, VC2, ID);
-  printf("Exemplo 2: ID = ");
-  for (i=0; i<NP; i++)
-     printf("%d ", ID[i]);
-  printf("\n");
-  return EXIT_SUCCESS;
+int main()
+{
+	unsigned int NP, NC;
+
+	printf("TEST 1: ");
+	NP = 2;
+	NC = 4;
+	int ID1[] = {-1, -1};
+	float VP1[] = {-1.0, 0.0, 2.7, 1.15};
+	double VC1[]= {-4.0, -1.0, 1.0, -3.89, 5.4, 0.51, 2.0, 1.0, 1.0, 3.0, 1.21, 0.9};
+	closestCircle(NP, VP1, NC, VC1, ID1);
+	cout(NP, ID1);
+
+	printf("TEST 2: ");
+	NP = 2;
+	NC = 3;
+	int ID2[] = {-1, -1};
+	float VP2[] = {-3.0, 1.0, -3.0, -1.5};
+	double VC2[]= {0.0, -6.0, 6.0, -10.0, 0.0, 10.75, 0.0, 0.0, 5.0};
+	closestCircle(NP, VP2, NC, VC2, ID2);
+	cout(NP, ID2);
+
+
+
+	return EXIT_SUCCESS;
 }
