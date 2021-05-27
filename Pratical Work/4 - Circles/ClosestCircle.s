17ecc05b93843c6b2c786e7f4725ad8ed6881803
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-27 14:41:19 +0100

    Update

--- Pratical Work/4 - Circles/ClosestCircle.s
+++ Pratical Work/4 - Circles/ClosestCircle.s
@@ -16,11 +16,13 @@
 closestCircle:  CBZ W0, Finish		// Se o número de pontos for nulo, fim
 				CBZ W2, Finish		// Se o número de círculos for nulo, fim
 				MOV X5, X3  		// É importante guardar o início do vector dos círculos, para ser usado sempre que há o "Search"
+				SUB X4, X4, #4		// Necessário para acerto de apontadores no Loop
 				LDR D11, max		// D11 fica com o valor máximo de distância, que vai ser alterando mediante a existência de valores mais baixos
 
 Loop:			CBZ W0, Finish		// Se chegou ao fim do vector de pontos, fim
 				SUB W0, W0, #1		// Senão, retira uma unidade ao contador
 				MOV X3, X5			// Ajustar X3 para ficar com o endereço do trio da primeira circunferência a verificar
+				ADD X4, X4, #4		// Ajustar o apontador dos IDs
 				LDR	S0, [X1], #4	// S0 --> X do ponto
 				LDR S1, [X1], #4	// S1 --> Y do ponto
 				FCVT D0, S0			// Tem de haver passagem de float para double, para compatibilidade das operações mais à frente
@@ -29,7 +31,7 @@ Loop:			CBZ W0, Finish		// Se chegou ao fim do vector de pontos, fim
 				MOV W7, 0			// W7 fica com o index do círculo que neste momento se vai pesquisar.
 				LDR D11, max		// D11 fica com um valor muito grande para ser facilmente substituido na primeira iteração.
 
-Search:			CBZ W6, Prep		// Se não houver mais círculos a procurar, volta ao loop para buscar o próximo ponto.
+Search:			CBZ W6, Loop		// Se não houver mais círculos a procurar, volta ao loop para buscar o próximo ponto.
 				LDR D2, [X3], #8	// D2 --> X do centro do círculo
 				LDR D3, [X3], #8	// D3 --> Y do centro do círculo
 				LDR D4, [X3], #8	// D4 --> R do centro do círculo
@@ -46,12 +48,8 @@ Search:			CBZ W6, Prep		// Se não houver mais círculos a procurar, volta ao lo
 				B.GT Jump			// Se D5 for maior que a anterior distância, não é válido para a solução. Pula.
 				FMOV D11, D5		// D11 fica atualmente com o menor valor de distância, que é o D5
 				STR W7, [X4]		// Posso já fazer o Store, no vector ID, do inteiro W7 que é o index do círculo mais próximo até agora.
-
 Jump:			SUB W6, W6, #1		// Faltam W6 iterações nos círculos para acabar o Search loop
 				ADD W7, W7, #1		// Próximo círculo, próximo index
 				B Search
 
-Prep:			ADD X4, X4, #4		// Preparação para ir buscar o próximo valor. Apenas aumentar o endereço X4 4 bytes, para ir para o próximo inteiro de ID.
-				B Loop
-
-Finish: 		RET					// Retorna. Void function, pois X0 não interessa para o resultado atual.
+Finish: 		RET					// Retorna "void function", pois X0 não interessa para o resultado final.
