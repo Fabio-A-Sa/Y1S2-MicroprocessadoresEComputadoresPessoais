6adaf4341ac4640633c3fa3bb94a76cbbf29a7d1
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-27 14:24:04 +0100

    Using D and C!

--- Pratical Work/4 - Circles/ClosestCircle.s
+++ Pratical Work/4 - Circles/ClosestCircle.s
@@ -3,7 +3,6 @@
 
 .data
 	max: .double 1000000
-	zero: .double 0
 .text
 .global closestCircle
 .type closestCircle, "function"
@@ -18,7 +17,6 @@ closestCircle:  CBZ W0, Finish		// Se o número de pontos for nulo, fim
 				CBZ W2, Finish		// Se o número de círculos for nulo, fim
 				MOV X5, X3  		// É importante guardar o início do vector dos círculos, para ser usado sempre que há o "Search"
 				LDR D11, max		// D11 fica com o valor máximo de distância, que vai ser alterando mediante a existência de valores mais baixos
-				MOV W10, W0			// W10 fica com o número de pontos, para ser usado com o vector ID
 
 Loop:			CBZ W0, Finish		// Se chegou ao fim do vector de pontos, fim
 				SUB W0, W0, #1		// Senão, retira uma unidade ao contador
@@ -29,32 +27,31 @@ Loop:			CBZ W0, Finish		// Se chegou ao fim do vector de pontos, fim
 				FCVT D1, S1			// Tem de haver passagem de float para double, para compatibilidade das operações mais à frente
 				MOV W6, W2			// W6 fica com o número de círculos a testar
 				MOV W7, 0			// W7 fica com o index do círculo que neste momento se vai pesquisar.
+				LDR D11, max		// D11 fica com um valor muito grande para ser facilmente substituido na primeira iteração.
 
-Search:			CBZ W6, Loop		// Se não houver mais círculos a procurar, volta ao loop para buscar o próximo ponto
+Search:			CBZ W6, Prep		// Se não houver mais círculos a procurar, volta ao loop para buscar o próximo ponto.
 				LDR D2, [X3], #8	// D2 --> X do centro do círculo
 				LDR D3, [X3], #8	// D3 --> Y do centro do círculo
 				LDR D4, [X3], #8	// D4 --> R do centro do círculo
-				FSUB D0, D0, D2
-				FSUB D1, D1, D3
-				FMUL D0, D0, D0
-				FMUL D1, D1, D1
-				FADD D0, D0, D1
-				FSQRT D0, D0
-				FSUB D0, D0, D4
-				FCMP D0, 0
-				B.LS Jump
-				FCMP D0, D11
-				B.GT Jump
-				LDR D1, zero
-				FADD D11, D0, D1 // D11 fica atualmente com o menor valor
-				STRB [X4]
+				FSUB D5, D0, D2		// D5 fica com D0-D2
+				FSUB D6, D1, D3		// D6 fica com D1-D3
+				FMUL D5, D5, D5		// D5 fica com (D0-D2)^2
+				FMUL D6, D6, D6		// D6 fica com (D1-D3)^2
+				FADD D5, D5, D6		// D5 fica com (D0-D2)^2 + (D1-D3)^2
+				FSQRT D5, D5		// D5 fica com SQRT((D0-D2)^2 + (D1-D3)^2)
+				FSUB D5, D5, D4		// D5 fica com SQRT((D0-D2)^2 + (D1-D3)^2) - D4
+				FCMP D5, 0
+				B.LE Jump			// Se D5 for menor ou igual que 0, então pertence ao círculo e não é válido para a solução. Pula.
+				FCMP D5, D11
+				B.GT Jump			// Se D5 for maior que a anterior distância, não é válido para a solução. Pula.
+				FMOV D11, D5		// D11 fica atualmente com o menor valor de distância, que é o D5
+				STR W7, [X4]		// Posso já fazer o Store, no vector ID, do inteiro W7 que é o index do círculo mais próximo até agora.
+
+Jump:			SUB W6, W6, #1		// Faltam W6 iterações nos círculos para acabar o Search loop
+				ADD W7, W7, #1		// Próximo círculo, próximo index
 				B Search
 
-Jump:
+Prep:			ADD X4, X4, #4		// Preparação para ir buscar o próximo valor. Apenas aumentar o endereço X4 4 bytes, para ir para o próximo inteiro de ID.
+				B Loop
 
-
-				SUB W6, W6, #1
-				SUB W10, W10, #1
-				B Search
-
-Finish: RET
+Finish: 		RET					// Retorna. Void function, pois X0 não interessa para o resultado atual.
