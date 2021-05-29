8130a6e8c42b743ddee01851d3511f9ffdf2be5c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-05-29 11:40:47 +0100

    Added file

--- Exercises/4 - SIMD Instructions/2 - Intern product.s
+++ Exercises/4 - SIMD Instructions/2 - Intern product.s
@@ -5,4 +5,13 @@
 intern: LSR W2, W2, 2
     MOV X3, 0
 Loop: CBZ X3, Finish
-    
+    LDR Q0, [X0], 16
+    LDR Q1, [X1], 16
+    MUL V2.4S, V1.4S, V0.4S
+    ADDV S3, V2.4S
+    SMOV X5, V3.S[0]
+    ADD X3, X3, X5
+    SUB W2, W2, 1
+    B Loop
+FInish: MOV X0, X3
+    RET
