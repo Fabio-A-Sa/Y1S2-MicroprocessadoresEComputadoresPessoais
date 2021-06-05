41328f8a670296495d53874271893efa07c92213
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-06-05 14:23:37 +0100

    Code

--- Pratical Work/5 - SIMD Instructions/histo.s
+++ Pratical Work/5 - SIMD Instructions/histo.s
@@ -19,24 +19,27 @@ loop: 		CBZ W0, Average					// Se chegou ao final do vector das notas, vai fazer
 Search: 	CMP W9, #0						// Se chegar a -1, volta ao main loop
 			B.LT loop
 			DUP V1.16B, W9					// Replicar o valor de W9 por todo o vector V4
-			CMEQ V2.16B, V0.16B, V1.16B		// Comparar cada byte dos vectores (V0 e V4), para ver se são iguais à nota pretendida. Se sim, fica o respectivo byte "a uns (1)".
-			ADDV B3, V2.16B					// B6 fica
-			MOV W4, V3.S[0]					// W4 fica com o número de ocorrências dentro daquela sequência de notas
-			NEG W4, W4
+			CMEQ V1.16B, V0.16B, V1.16B		// Comparar cada byte dos vectores (V0 e V1), para ver se são iguais à nota pretendida. Se sim, fica o respectivo byte "a uns (1)".
+			ADDV B2, V1.16B					// B2 fica com o número complementar da frequência dessa nota no vector
+			MOV W4, V2.S[0]					// W4 fica com o número de ocorrências dentro daquela sequência de notas (mas ainda é um número negativo!)
+			NEG W4, W4						// -(-W4) = W4 (valor positivo)
 			LDRB W3, [X2, X9]				// Buscar o valor antigo do Histograma, W3
 			ADD W4, W4, W3					// Adicionar W3 à frequência absoluta obtida neste conjunto de 16 notas, W4
-			STRB W4, [X2, X9]				// Fazer store desse novo valor no histograma
+			STRB W4, [X2, X9]				// Fazer store desse novo valor no mesmo local do Histograma
 			SUB W9, W9, #1					// Agora é altura de verificar quantos W9-1 existem no conjunto de 16 notas, voltando ao Search
 			B Search
 
-Average: 	MOV X5, #20						// X5 fica responsável pelo offset dentro do Histograma (que também coincide, em cada byte, com o valor da nota associada)
+Average: 	MOV W5, #20						// X5 fica responsável pelo offset dentro do Histograma (que também coincide, em cada byte, com o valor da nota associada)
 			MOV X0, #0						// X0 será a soma total de todos os valores que o histograma representa
 
-Grades:  	CBZ X5, Finish					// Se chegar ao fim, faz a média em "Finish"
+GetGrades:  CBZ W5, Finish					// Se chegar ao fim, faz a média em "Finish"
 			LDRB W4, [X2, X5]				// Load de um byte com offset X2-X5 no vector H
 			MUL X4, X4, X5					// Número de valores vezes o número de alunos que obtiveram essa classificação
 			ADD X0, X0, X4					// Juntar ao acumulador
-			SUB X5, X5, #1					// Agora só faltam X5-1 notas a somar
-			B Grades
+			SUB W5, W5, #1					// Agora só faltam X5-1 notas a somar
+			B GetGrades
 
-Finish: 	RET
+Finish: 	SCVTF S7, W7					// Int --> Float (precisão simples, S)
+			SCVTF S0, W0					// Int --> Float (precisão simples, S)
+			FDIV S0, S0, S7					// S0 <-- Número total de notas / Número total de alunos
+			RET								// Valor a retornar em S0 (no código C o "histo" retorna valor em formato float)
